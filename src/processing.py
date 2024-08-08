1b503c59c2dcd43a877fbc292ef383ee90716ba8
Artyom_Kazagashev <tema124ru@mail.ru>
2024-08-08 12:56:18 +0700

    add filter

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
-def filter_by_state(filt, state="EXECUTED"):
+def filter_by_state(filter, state="EXECUTED"):
     filtered_date = []
-    for item in filt:
+    for item in filter:
         if item.get("state") == state:
             filtered_date.append(item)
     return filtered_date

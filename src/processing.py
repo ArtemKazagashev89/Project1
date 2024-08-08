2eb4cb53d2bbda6fc0738212de39645a1ca86ebb
Artyom_Kazagashev <tema124ru@mail.ru>
2024-08-08 15:12:56 +0700

    add sorted and fixes

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,36 @@
-def filter_by_state(filter, state="EXECUTED"):
+def filter_by_state(date, state="EXECUTED"):
     filtered_date = []
-    for item in filter:
+    for item in date:
         if item.get("state") == state:
             filtered_date.append(item)
     return filtered_date
+
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+
+
+def sort_by_date(date_list: list, reverse_list: bool = True) -> list:
+    """Return filtered list by date"""
+    sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
+    return sorted_list
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
